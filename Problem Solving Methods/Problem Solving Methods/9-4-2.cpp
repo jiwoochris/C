a858828766db7b82c0f27b306e35893f07374c4e
juicyjung <travelandi01@gmail.com>
2021-09-17 13:45:51 +0900

    fix something

--- Problem Solving Methods/Problem Solving Methods/9-4-2.cpp
+++ Problem Solving Methods/Problem Solving Methods/9-4-2.cpp
@@ -1,23 +1,24 @@
 #include <stdio.h>
+#include <math.h>
 
-void liquid(int number, float* gallons, float* quarts, float* pints, float* cups) {
-	*pints = number / float(2);
-	*quarts = number / float(4);
-	*gallons = number / float(16);
-	*cups = number;
+void liquid(int number,int* gallons, int* quarts, int* pints, int* cups) {
+	*pints = ceil(number / float(2));
+	*quarts = ceil(number / float(4));
+	*gallons = ceil(number / float(16));
+	*cups = ceil(number);
 }
 
 
 int main() {
 	int number[] = { 5, 34, 12, 0 };
-	float gallons, quarts, pints, cups;
+	int gallons, quarts, pints, cups;
 
 	for (int i = 0; i < 4; i++) {
 		liquid(number[i], &gallons, &quarts, &pints, &cups);
 
 		printf("\n");
 		printf(" the total number of cups : %d", number[i]);
-		printf(" ==> gallons : %f, quarts : %f, pints : %f, cups : %f", gallons, quarts, pints, cups);
+		printf(" ==> gallons : %d, quarts : %d, pints : %d, cups : %d", gallons, quarts, pints, cups);
 		printf("\n");
 	}
 
